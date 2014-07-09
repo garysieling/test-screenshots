b6b7b2a57454ddf905592b730db57b21fa12e763
gsieling <gsieling@wingspan.com>
2014-07-08 20:14:49 -0400

    add ability to detect file differences

--- diff.sh
+++ diff.sh
@@ -1,8 +1,23 @@
 echo $1
 echo $2
 
+export file1=`pwd`/$1 
+export file2=`pwd`/$2 
 
-compare `pwd`/$1 `pwd`/$2 `pwd`/diff.png \
+echo $file1
+echo $file2
+
+export cf=`compare -metric MAE $file1 $file2 null: 2>&1`
+
+echo $cf
+if [ "$cf" = "0 (0)" ]
+then
+  echo "Files are equal"
+else
+  echo "Files are not equal"
+fi
+
+compare $file1 $file2 `pwd`/diff.png \
           -compose Src -highlight-color White -lowlight-color Black \
           diff.png
 
